6a8d86b30e8ba7fa59b1a36f49f2205e6e2682f8
Akash Sil <54166450+Akash420-oss@users.noreply.github.com>
2024-10-29 10:57:06 +0530

    Add files via upload

---
+++ install.sh
@@ -0,0 +1,7 @@
+sudo mkdir /usr/share/doc/dare-devil/
+sudo mv *.h /usr/include/ && sudo mv dare-devil.desktop /usr/share/applications/ && sudo mv  Dare_Devil.png /usr/share/pixmaps && sudo mv LICENSE /usr/share/doc/dare-devil/ && sudo mv README.md /usr/share/doc/dare-devil/ && sudo mv devil_&angel.gif /usr/share/doc/dare-devil/
+sudo gcc -o dare_devil dare_devil.c -w
+sudo rm -f dare_devil.c
+sudo mv dare_devil /usr/bin/
+sudo chmod +x dare_devil
+
---
+++ uninstall.sh
@@ -0,0 +1,1 @@
+sudo rm -rf /usr/share/doc/dare-devil/ && sudo rm -f /usr/share/applications/dare-devil.desktop /usr/share/pixmaps/Dare_Devil.png /usr/bin/dare_devil
