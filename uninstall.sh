584f5976ebb5b06fa2ce9d040ad4127cf42ac603
Akash Sil <54166450+Akash420-oss@users.noreply.github.com>
2024-10-29 11:02:12 +0530

    uninstall.sh

--- uninstall.sh
+++ uninstall.sh
@@ -1,1 +1,2 @@
 sudo rm -rf /usr/share/doc/dare-devil/ && sudo rm -f /usr/share/applications/dare-devil.desktop /usr/share/pixmaps/Dare_Devil.png /usr/bin/dare_devil
+sudo rm -f /usr/include/devil_art.h /usr/include/custom_fun.h  /usr/include/system_crash.h /usr/include/create_soul_data.h
