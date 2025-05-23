7512cb35bf7499bb18b7f9e8c9ef558950668ca6
Akash Sil <54166450+Akash420-oss@users.noreply.github.com>
2025-05-23 15:25:24 +0530

    install.sh

--- install.sh
+++ install.sh
@@ -1,3 +1,4 @@
+rm -f dare_devil
 sudo mkdir /usr/share/doc/dare-devil/ && sudo mkdir /usr/share/dare-devil/
 sudo mv *.h /usr/include/ && sudo mv dare-devil.desktop /usr/share/applications/ && sudo mv  Dare_Devil.png /usr/share/pixmaps && sudo mv LICENSE /usr/share/doc/dare-devil/ && sudo mv README.md /usr/share/doc/dare-devil/ && sudo mv 'devil_&angel.gif' /usr/share/doc/dare-devil/
 sudo gcc -o dare_devil dare_devil.c -w
